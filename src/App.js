795d47f512698a9a62a5b2fd1f36aef620c3cdc2
Alvin Yee <alvinec09@yahoo.com>
2019-03-15 15:38:23 -0700

    Get onchange callback to work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: ''
+    };
+  }
+
+  changetext(e) {
+    this.setState({
+      searchText: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>Movie Search App</h1>
+      <input
+        type="text"
+        onChange={(e) => this.changetext(e)}/>
+        <p>{this.state.searchText}</p>
       </div>
     );
   }
